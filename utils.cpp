d58908e6d9198e866d99c04928e3e0f988a93eb7
BAllanach <B.C.Allanach@damtp.cam.ac.uk>
2013-02-22 15:19:54 +0000

    Made the close function more robust

--- utils.cpp
+++ utils.cpp
@@ -66,8 +66,11 @@ bool testNan(double f) {
 }
 
 bool close(double m1, double m2, double tol) {
-  return (fabs(maximum(fabs(m1), fabs(m2)) - fabs(minimum(fabs(m1), fabs(m2))))
-	  <= tol * maximum(fabs(m1), fabs(m2)));
+  double mmax = fabs(maximum(fabs(m1), fabs(m2)));
+  double mmin = fabs(minimum(fabs(m1), fabs(m2)));
+
+  if (mmax < EPSTOL) return 1; 
+  return (mmax - mmin <= tol * mmax);
 }
 
 
