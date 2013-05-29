edccbe02d6549e7ed0121cc4aca2d02abbb02cdb
BAllanach <B.C.Allanach@damtp.cam.ac.uk>
2013-05-29 17:12:34 +0100

    At least it compiles apart from some external variables not put into main program: Newton coded, needs checking

--- numerics.cpp
+++ numerics.cpp
@@ -1352,7 +1352,7 @@ double fmin(DoubleVector x) {
 void lnsrch(int n, DoubleVector xold, double fold, DoubleVector g, 
 	    DoubleVector p, 
 	    DoubleVector & x, double & f, double stpmax, int & check, 
-	    double (*func)(DoubleVector)) {
+	    double (*func)(DoubleVector x)) {
   const double ALF = 1.0e-4;
   const double TOLX = 1.0e-7;
   
@@ -1479,75 +1479,71 @@ void ludcmp(DoubleMatrix & a, int n, int *indx, double & d) {
 }
 
 
-/*void newt(double x[], int n, int *check,
-	void (*vecfunc)(int, double [], double [])) {
+void newt(DoubleVector x, int n, int & check,
+	  void (*vecfunc)(int, DoubleVector, DoubleVector &)) {
   const int    MAXITS = 200;
   const double TOLF   = 1.0e-4;
   const double TOLMIN = 1.0e-6;
   const double TOLX   = 1.0e-7;
   const double STPMX  = 100.0;
   
-  void lnsrch(int n, double xold[], double fold, double g[], double p[], double x[],
-	      double *f, double stpmax, int *check, double (*func)(double []));
-  void lubksb(double **a, int n, int *indx, double b[]);
-  void ludcmp(double **a, int n, int *indx, double *d);
   int i,its,j,*indx;
-  double d,den,f,fold,stpmax,sum,temp,test,**fjac,*g,*p,*xold;
+  double d,den,f,fold,stpmax,sum,temp,test;
   
-	indx=ivector(1,n);
-	fjac=matrix(1,n,1,n);
-	g=vector(1,n);
-	p=vector(1,n);
-	xold=vector(1,n);
-	fvec=vector(1,n);
-	nn=n;
-	nrfuncv=vecfunc;
-	f=fmin(x);
-	test=0.0;
-	for (i=1;i<=n;i++)
-		if (fabs(fvec[i]) > test) test=fabs(fvec[i]);
-	if (test < 0.01*TOLF) {
-		*check=0;
-		FREERETURN
-	}
-	for (sum=0.0,i=1;i<=n;i++) sum += SQR(x[i]);
-	stpmax=STPMX*FMAX(sqrt(sum),(double)n);
-	for (its=1;its<=MAXITS;its++) {
-		fdjac(n,x,fvec,fjac,vecfunc);
-		for (i=1;i<=n;i++) {
-			for (sum=0.0,j=1;j<=n;j++) sum += fjac[j][i]*fvec[j];
-			g[i]=sum;
-		}
-		for (i=1;i<=n;i++) xold[i]=x[i];
-		fold=f;
-		for (i=1;i<=n;i++) p[i] = -fvec[i];
-		ludcmp(fjac,n,indx,&d);
-		lubksb(fjac,n,indx,p);
-		lnsrch(n,xold,fold,g,p,x,&f,stpmax,check,fmin);
-		test=0.0;
-		for (i=1;i<=n;i++)
-			if (fabs(fvec[i]) > test) test=fabs(fvec[i]);
-		if (test < TOLF) {
-			*check=0;
-			FREERETURN
-		}
-		if (*check) {
-			test=0.0;
-			den=FMAX(f,0.5*n);
-			for (i=1;i<=n;i++) {
-				temp=fabs(g[i])*FMAX(fabs(x[i]),1.0)/den;
-				if (temp > test) test=temp;
-			}
-			*check=(test < TOLMIN ? 1 : 0);
-			FREERETURN
-		}
-		test=0.0;
-		for (i=1;i<=n;i++) {
-			temp=(fabs(x[i]-xold[i]))/FMAX(fabs(x[i]),1.0);
-			if (temp > test) test=temp;
-		}
-		if (test < TOLX) FREERETURN
-	}
-	throw("MAXITS exceeded in newt");
+  indx=ivector(1,n);
+  DoubleMatrix fjac(n, n);
+  DoubleVector g(n), p(n), xold(n);
+  NR::fvec = DoubleVector(n);
+  NR::nn=n;
+  NR::nrfuncv=vecfunc;
+  f=fmin(x);
+  test=0.0;
+  for (i=1;i<=n;i++)
+    if (fabs(NR::fvec(i)) > test) test=fabs(NR::fvec(i));
+  if (test < 0.01*TOLF) {
+    check=0;
+    free_ivector(indx,1,n);return;
+  }
+  for (sum=0.0,i=1;i<=n;i++) sum += sqr(x(i));
+  stpmax=STPMX*maximum(sqrt(sum),(double)n);
+  for (its=1;its<=MAXITS;its++) {
+    fdjac(n,x,NR::fvec,fjac,vecfunc);
+    for (i=1;i<=n;i++) {
+      for (sum=0.0,j=1;j<=n;j++) sum += fjac(j, i)*NR::fvec(j);
+      g(i)=sum;
+    }
+    for (i=1;i<=n;i++) xold(i)=x(i);
+    fold=f;
+    for (i=1;i<=n;i++) p(i) = -NR::fvec(i);
+    ludcmp(fjac, n, indx, d);
+    lubksb(fjac, n, indx, p);
+    lnsrch(n, xold, fold, g, p, x, f, stpmax, check, fmin);
+    test=0.0;
+    for (i=1;i<=n;i++)
+      if (fabs(NR::fvec(i)) > test) test=fabs(NR::fvec(i));
+    if (test < TOLF) {
+      check=0;
+      free_ivector(indx,1,n);
+      return;
+    }
+    if (check) {
+      test=0.0;
+      den=maximum(f,0.5*n);
+      for (i=1;i<=n;i++) {
+	temp=fabs(g(i))*maximum(fabs(x(i)),1.0)/den;
+	if (temp > test) test=temp;
+      }
+      check=(test < TOLMIN ? 1 : 0);
+      free_ivector(indx,1,n);
+      return;
+    }
+    test=0.0;
+    for (i=1;i<=n;i++) {
+      temp=(fabs(x(i)-xold(i)))/maximum(fabs(x(i)),1.0);
+      if (temp > test) test=temp;
+    }
+    if (test < TOLX) { free_ivector(indx,1,n); return; }
+  }
+  throw("MAXITS exceeded in newt");
 }
-*/
+
