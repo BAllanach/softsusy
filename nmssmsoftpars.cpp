522198cbf04056146a3f3ee51d98787fdc528c15
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-08-28 17:48:25 +0200

    Set mS'^2 to m0^2 in the universal scalar GUT condition

--- nmssmsoftpars.cpp
+++ nmssmsoftpars.cpp
@@ -660,6 +660,7 @@ void SoftParsNmssm::anomalousDeriv(DoubleMatrix & gEE, DoubleMatrix & gLL,
 void SoftParsNmssm::universalScalars(double m0) {
   SoftPars<NmssmSusy, nmsBrevity>::universalScalars(m0);
   setMsSquared(sqr(m0));
+  setMspSquared(sqr(m0));
 }
 
 //PA: for semi constrained models
