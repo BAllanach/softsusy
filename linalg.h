cb4337e4ff46a713497ad13e1382df921a07ad74
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-10-14 11:21:38 +0200

    Bugfix: avoid undefined behaviour in vector / matrix assignment
    The default operator= for vectors and matrices uses the
    valarray<>::operator= to assign the the elements.  However, if the the
    origin and the target valarrays have different size, the behaviour of
    valarray<>::operator= is undefined.  This happens for example in the
    NMSSM, where we resize matrices and assign them to smaller ones.
    
    To solve the problem we have to overwrite the default operator= for
    all vectors and matrices to ensure that the target valarray is resized
    properly before the assignment.

--- linalg.h
+++ linalg.h
@@ -64,6 +64,14 @@ public:
     return *this;
   }
 
+  DoubleVector& operator=(const DoubleVector& other) {
+    x.resize(other.x.size());
+    x = other.x;
+    start = other.start;
+    end = other.end;
+    return *this;
+  }
+
   template <typename E>
   DoubleVector(const Xpr<double,E> & v)
     : Indexable<double,DoubleVector>(), 
@@ -246,6 +254,14 @@ public:
   template<class E>
   DoubleMatrix & operator=(const MatXpr<double,E> & x) {
     *this = copy_from(x);
+    return *this;
+  }
+
+  DoubleMatrix& operator=(const DoubleMatrix& other) {
+    x.resize(other.x.size());
+    x = other.x;
+    rows = other.rows;
+    cols = other.cols;
     return *this;
   }
 
@@ -488,6 +504,14 @@ public:
     return *this;
   }
 
+  ComplexVector& operator=(const ComplexVector& other) {
+    x.resize(other.x.size());
+    x = other.x;
+    start = other.start;
+    end = other.end;
+    return *this;
+  }
+
   template <typename E>
   ComplexVector(const Xpr<Complex,E> & v)
     : Indexable<Complex,ComplexVector>(), 
@@ -624,6 +648,14 @@ public:
   template<class E>
   ComplexMatrix & operator=(const MatXpr<Complex,E> & x) {
     *this = copy_from(x);
+    return *this;
+  }
+
+  ComplexMatrix& operator=(const ComplexMatrix& other) {
+    x.resize(other.x.size());
+    x = other.x;
+    rows = other.rows;
+    cols = other.cols;
     return *this;
   }
 
