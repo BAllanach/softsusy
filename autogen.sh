4379fac3d0a42391fb78fedfc82a1fd4d2567e61
Lewis Tunstall <lewistunstall@itp62.unibe.ch>
2013-11-18 10:38:26 +0100

    Updated autogen.sh with Alex's changes for Mac OS X users.  Tested on Lewis' and Tony's Macs (it works!)

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,53 @@ DIE=0
 PROJECT="Softsusy"
 CONFIG_DIR="Config"
 
+exists_in_path() {
+    # This function will try to locate an executable [$1] in $PATH.
+    #
+    # The result of the search is stored in cmd, which should be
+    # immediately copied, since the variables value will be
+    # overwritten at next invocation of this function.
+
+    # Assert that we got enough arguments
+    if test $# -ne 1 ; then
+        echo "exists_in_path: Exactly one argument required"
+        return 1
+    fi
+
+    cmd=$(command -v -- "$1")
+    case "$cmd" in
+	/*) return 0 ;;
+	alias\ *) return 1 ;; # alias
+	*) return 1 ;; # built-in or function
+    esac
+}
+
+call_libtoolize() {
+    # This function calls libtoolize.
+    libtoolize_cmd="libtoolize"
+
+    # on Darwin systems prefer glibtoolize over libtoolize
+    case `uname` in
+        Darwin*)
+            exists_in_path glibtoolize
+            if [ -n "$cmd" ] ; then
+                libtoolize_cmd="glibtoolize"
+            else
+                libtoolize_cmd="libtoolize"
+            fi
+            ;;
+    esac
+
+    exists_in_path "${libtoolize_cmd}"
+    if [ -n "$cmd" ] ; then
+        ${libtoolize_cmd} -c -f
+    else
+        echo "Error: ${libtoolize_cmd} not found."
+        echo "You must have libtool installed to run this script."
+        exit 1
+    fi
+}
+
 (aclocal --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have aclocal installed to compile $PROJECT."
@@ -50,8 +97,7 @@ then
 fi
 autoheader
 # create file Config/ltmain.sh
-case `uname` in Darwin*) glibtoolize -c -f ;;
-  *) libtoolize -c -f ;; esac
+call_libtoolize
 automake --add-missing --copy
 autoconf
 
