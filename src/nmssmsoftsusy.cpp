3df8843f2082da0150b84c570b6816b40ceef450
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-11-27 17:37:57 +0100

    move Z3 sanity check to lowOrg to avoid Z3 violation in lowOrg

--- src/nmssmsoftsusy.cpp
+++ src/nmssmsoftsusy.cpp
@@ -8432,16 +8432,6 @@ void NmssmSoftsusy::itLowsoft
   /// On first iteration, don't bother with finite corrections
   
   numTries = numTries + 1;
-  
-  //PA: reset new low energy inputs of general nmssm at mz.
-  if (Z3) {
-    if (PRINTOUT && !close(nmpars(4), 0.0, EPSTOL))
-      cout << "WARNING: you set Z3 == true and xiF != 0, xiF will be ignored\n";
-    if (PRINTOUT && !close(nmpars(5), 0.0, EPSTOL))
-      cout << "WARNING: you set Z3 == true and mu' != 0, mu' will be ignored\n";
-    setXiF(0.0);
-    setMupr(0.0);
-  }
 
   try {
     sparticleThresholdCorrections(tanb); 
@@ -8647,6 +8637,15 @@ void NmssmSoftsusy::lowOrg
  double mxGuess,  const DoubleVector & pars, const DoubleVector nmpars, 
  int sgnMu, double tanb, const QedQcd & oneset, bool gaugeUnification, 
  bool ewsbBCscale) {
+
+  if (Z3) {
+    if (PRINTOUT && !close(nmpars(4), 0.0, EPSTOL))
+      cout << "WARNING: you set Z3 == true and xiF != 0, xiF will be ignored\n";
+    if (PRINTOUT && !close(nmpars(5), 0.0, EPSTOL))
+      cout << "WARNING: you set Z3 == true and mu' != 0, mu' will be ignored\n";
+    setXiF(0.0);
+    setMupr(0.0);
+  }
 
   try {
      
