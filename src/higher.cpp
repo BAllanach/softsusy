1ae89f19182f59308093bd52f5cded08bc15c022
BAllanach <B.C.Allanach@damtp.cam.ac.uk>
2016-01-13 09:57:52 +0000

    Added some of Steve's mods to higher.cpp

--- src/higher.cpp
+++ src/higher.cpp
@@ -7,13 +7,17 @@
                 arXiv:16??.?????
    Webpage:     http://allanach.home.cern.ch/allanach/softsusy.html
    Description: main calling program to be put in src/ in the softsusy
-                directory. It produces data listed in "twoLoop.dat"
-		illustrating the size of two-loop contributions to gluino and
-		squark masses. You'll need to install nllfast in some
-		directory and change the reference to
-		/home/bca20/code/nllfast-3.0-13TeV to the directory you've
-		placed it in. The executable will be called "higher.x" and
-		takes no arguments.
+                directory. Running make will produce an executable called
+                "higher.x", which takes no arguments. Running "higher.x"
+                produces the data given in "twoLoop.dat", which were used to 
+                make the figures in the manual, and which illustrate the size 
+                of two-loop contributions to gluino and squark masses and the
+                resulting corrections to gluino and squark production 
+                cross-sections at the 13 TeV LHC. You will need to install 
+                NLL-fast (google it) in some directory, and compile it to get 
+                an executable which you should call nllfast13. Then change 
+                the (two) references to /home/bca20/code/nllfast-3.0-13TeV 
+                below to point to the directory you've placed nllfast13 in. 
 */
 
 #include <iostream>
@@ -42,14 +46,14 @@ void getCrossSection(MssmSoftsusy & r, double m0, double m12, double a0,
 		r.displayPhys().mu(1, 2) + r.displayPhys().md(1, 2)) * 0.25;
 
   char buff[500];
-  sprintf(buff, "cd /home/bca20/code/nllfast-3.0-13TeV; ./nllfast13 gg mstw %f %f > output 2> err; ./nllfast13 sg mstw %f %f >> output 2>> err; ./nllfast13 ss mstw %f %f >> output 2>> err; ./nllfast13 sb mstw %f %f >> output 2>> err; ./nllfast13 st mstw %f >> output 2>> err",mq,mg,mq,mg,mq,mg,mq,mg,mt1);
+  sprintf(buff, "cd /home/bca20/code/nllfast-3.1-13TeV; ./nllfast13 gg mstw %f %f > output 2> err; ./nllfast13 sg mstw %f %f >> output 2>> err; ./nllfast13 ss mstw %f %f >> output 2>> err; ./nllfast13 sb mstw %f %f >> output 2>> err; ./nllfast13 st mstw %f >> output 2>> err",mq,mg,mq,mg,mq,mg,mq,mg,mt1);
   //  cout << buff << endl;
   int err = system(buff);
   xsGG = 0.; xsSG = 0.; xsSS = 0.; xsSB = 0.; xsTB = 0.;
 
   char c[500];
   char fn[500];
-  sprintf(fn, "/home/bca20/code/nllfast-3.0-13TeV/output");
+  sprintf(fn, "/home/bca20/code/nllfast-3.1-13TeV/output");
   if (!err) {
     fstream fin(fn, ios::in); 
     fin >> c >> c >> c >> c >> c >> c >> c >> c >> c >> c >> c >> c >> c 
@@ -119,6 +123,8 @@ void scaleVariation() {
 
 void doScan(double lowRatio, double highRatio, int numPoints) {
   cout << "# Data file for plots\n";
+  cout << "# Figure 3(a) is columns 4, 7, and 10 vs. column 1.\n";
+  cout << "# Figure 3(b) is columns 39, 42, 45, 48, and 51 vs. column 1.\n";
   cout << "# m0/M12        m_gl(1-loop)    m_gl(2-loop)    muL(1-loop)     muL(2-loop)     mt1(1-loop)     mt1(2-loop)     mt2(1-loop)     mt2(2-loop)     mdL(1-loop)     mdL(2-loop)     mb1(1-loop)     mb1(2-loop)     muR(1-loop)     muR(2-loop)     muR(1-loop)     muR(2-loop)     mb2(1-loop)     mb2(2-loop)     M3(MSUSY)       muL(MSUSY)      mtL(MSUSY)      mtR(MSUSY)      mdL(MSUSY)      mbL(MSUSY)      muR(MSUSY)      mdR(MSUSY)      mbR(MSUSY)      mt(MSUSY)       mneut1(MSUSY)   mneut2(MSUSY)   mneut3(MSUSY)    mneut4(MSUSY)   gg(1-loop)      gg(2-loop)      sg(1-loop)      sg(2-loop)      ss(1-loop)      ss(2-loop)      sb(1-loop)      sb(2-loop)      tb(1-loop)      tb(2-loop)\n";
 
     /// Sets format of output: 6 decimal places
@@ -156,8 +162,6 @@ void doScan(double lowRatio, double highRatio, int numPoints) {
       USE_TWO_LOOP_SPARTICLE_MASS = true;
       ho.lowOrg(sugraBcs, mGutGuess, pars, sgnMu, tanb, oneset, uni, 
 		ewsbBCscale);
-      //      ho.lesHouchesAccordOutput(cout, modelIdent, pars, sgnMu, tanb, qMax, 
-      //			       0, ewsbBCscale);
 
       if (r.displayProblem().test()) cout << "# ";
       cout << m0Overm12 << " "                 // 1
@@ -203,8 +207,8 @@ void doScan(double lowRatio, double highRatio, int numPoints) {
       getCrossSection(r, m0, m12, a0, tanb, xsGG, xsSG, xsSS, xsSB, xsTB);
       getCrossSection(ho, m0, m12, a0, tanb, xsGGho, xsSGho, xsSSho, xsSBho, 
 		      xsTBho);
-      cout 
-	<< xsGG << " "   // 34
+
+      cout << xsGG << " "   // 34
 	<< xsGGho << " " // 35
 	<< xsSG << " "   // 36
 	<< xsSGho << " " // 37
@@ -228,28 +232,50 @@ int main(int argc, char *argv[]) {
 
   TOLERANCE = 1.0e-4;
   try {
-    doScan(0.1, 4.5, 20); cout << endl << endl;
+/* This is what is currently in Figure 3 of the present paper.
+*/
+//    doScan(0.1, 4.5, 20); cout << endl << endl;
 
+/* A higher resolution scan. Use this for the paper instead? 
+   Much slower of course...
+    doScan(0.1, 3.3, 320); cout << endl << endl;
+*/
+
+/* These should agree at the point m0/mhalf = 1.76, but they all give different
+   results for delta(mstop1)/mstop1 there! Bug??!? */
+    doScan(0.16, 1.76, 16); cout << endl << endl;
+    doScan(1.60, 1.76, 16); cout << endl << endl;
+    doScan(1.66, 1.76, 10); cout << endl << endl;
+    doScan(1.72, 1.76, 2); cout << endl << endl;
+
+
+    cout << "# Figure 4, tree-level gluino mass\n";
     cout << "# Q/MSUSY       Q/GeV           M3(Q)\n";
     treeLevelGluino = true;
     USE_TWO_LOOP_SPARTICLE_MASS = false; 
     scaleVariation(); 
     treeLevelGluino = false;
     USE_TWO_LOOP_SPARTICLE_MASS = false;
+    cout << "# Figure 4, 1-loop gluino pole mass\n";
     cout << "# 1-loop result\n# Q/MSUSY       Q/GeV           mgluinopole\n";
 
     scaleVariation(); 
     USE_TWO_LOOP_SPARTICLE_MASS = true;
-    expandAroundGluinoPole = 0;
-    cout << "# 2-loop result: no expansion\n# Q/MSUSY       Q/GeV           mgluinopole\n";
-    scaleVariation(); 
+
     expandAroundGluinoPole = 1;
-    cout << "# 2-loop result: full expansion\n# Q/MSUSY       Q/GeV           mgluinopole\n";
-    scaleVariation(); 
-    expandAroundGluinoPole = 2;
-    cout << "# 2-loop result: gluino expansion\n# Q/MSUSY       Q/GeV           mgluinopole\n";
+    cout << "# Figure 4, 2-loop gluino pole mass, re-expand around gluino, squark poles\n";
+    cout << "# Q/MSUSY       Q/GeV           mgluinopole\n";
     scaleVariation(); 
 
+    expandAroundGluinoPole = 0;
+    cout << "# Figure 4, 2-loop gluino pole mass, no re-expansion\n";
+    cout << "# Q/MSUSY       Q/GeV           mgluinopole\n";
+    scaleVariation(); 
+
+    expandAroundGluinoPole = 2;
+    cout << "# Figure 4, 2-loop gluino pole mass, re-expand around gluino pole\n";
+    cout << "# Q/MSUSY       Q/GeV           mgluinopole\n";
+    scaleVariation(); 
   }
   catch(const string & a) { cout << a; return -1; }
   catch(const char * a) { cout << a; return -1; }
