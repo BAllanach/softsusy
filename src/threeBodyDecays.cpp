ba27722049ed0d7c33bf11952df68fc7cbb039ef
BAllanach <B.C.Allanach@damtp.cam.ac.uk>
2017-03-26 21:43:43 +0200

    Commented contents of threeBodyDecays to avoid warning messages

--- src/threeBodyDecays.cpp
+++ src/threeBodyDecays.cpp
@@ -1,4 +1,4 @@
-/** \file decays.cpp
+/** \file threeBodyDecays.cpp
    Project:     SOFTSUSY 
    Author:      Tom Cridge, Ben Allanach
    Manual:      hep-ph/0104145, Comp. Phys. Comm. 143 (2002) 305 
@@ -14,7 +14,7 @@
 #include "main.h"
 #include <complex>
 
-static double m1 = 0.,m2 = 0.,m3 = 0.,m4 = 0.,mq = 0.,m5 = 0.,m6 = 0.,
+/*static double m1 = 0.,m2 = 0.,m3 = 0.,m4 = 0.,mq = 0.,m5 = 0.,m6 = 0.,
   m7 = 0., m8 = 0., MZboson = 0., MWboson = 0., mh = 0., mH = 0.,
-  mA = 0., mphi = 0., g1 = 0., g2 = 0., alphamix = 0., betavac = 0.;
+  mA = 0., mphi = 0., g1 = 0., g2 = 0., alphamix = 0., betavac = 0.;*/
 
