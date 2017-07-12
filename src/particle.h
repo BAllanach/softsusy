870becaa361c57c14b54e0b419a4c24726aa8ead
BAllanach <B.C.Allanach@damtp.cam.ac.uk>
2017-07-12 15:43:22 +0100

    Just added a few comments

--- src/particle.h
+++ src/particle.h
@@ -10,22 +10,22 @@
 #ifndef PARTICLE_H
 #define PARTICLE_H
 
-/// Particle class definition
+/// Particle class definition for decays
 class Particle {
  public:
-  string name;
-  double mass;
-  double PDG;
-  double No_of_Decays;
-  double No_1to2_Decays;
-  double No_1to3_Decays;
-  double No_grav_Decays;
-  double No_NMSSM_Decays;
-  double total_width;
-  double two_width;
-  double three_width;
-  vector<vector<double> > Array_Decays;
-  vector<string> Array_Comments;
+  string name;                             ///< name of the parent
+  double mass;                             ///< mass of the parent
+  double PDG;                              ///< PDG code of parent
+  double No_of_Decays;                     ///< How many decay modes
+  double No_1to2_Decays;                   ///< How many 2-body decay modes
+  double No_1to3_Decays;                   ///< How many 3-body decay modes
+  double No_grav_Decays;                   ///< How many decays to gravitinos
+  double No_NMSSM_Decays;                  ///< How many NMSSM specific modes
+  double total_width;                
+  double two_width;                        ///< Total width of 2-body modes
+  double three_width;                      ///< Total width of 3-body modes
+  vector<vector<double> > Array_Decays;    ///< Partial width for each decay
+  vector<string> Array_Comments;           ///< Comments for SLHA file
 };
 
 #endif
