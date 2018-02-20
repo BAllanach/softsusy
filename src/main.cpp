e48b7c5d516f35ef795117a2115155d638ffa30d
BAllanach <B.C.Allanach@damtp.cam.ac.uk>
2018-02-20 15:01:53 +0000

    Gone back to no himalaya in output

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,9 @@
    File:        main.cpp
    Author:      Ben Allanach 
    Manual:      B.C. Allanach, hep-ph/0104145, Comp. Phys. Comm. 143 (2002) 305 
-   Webpage:     http://softsusy.hepforge.org/
+                B.C. Allanach, M.A. Bernhardt, arXiv:0903.1805, Comp. Phys. 
+		Commun. 181 (2010) 232-245
+   Webpage:     http://allanach.home.cern.ch/allanach/softsusy.html
    Description: main calling program example: performs a scan of tan beta 
    (starting at CMSSM10.1.1) and prints out Higgs masses as a result in
    the format
@@ -23,22 +25,21 @@ int main() {
   try {
     /// Sets format of output: 6 decimal places
     outputCharacteristics(6);
-
+    
     cout << "# SOFTSUSY" << PACKAGE_VERSION 
 	 << " test program, Ben Allanach 2002\n";
     cout << "# If you use SOFTSUSY, please refer to B.C. Allanach,\n";
     cout << "# Comput. Phys. Commun. 143 (2002) 305, hep-ph/0104145\n";
     
-    /// Parameters used: CMSSM parameters to get a high mh
-    double m12 = 5000., a0 = -14.e3, mGutGuess = 2.0e16,
-      tanb = 20.0, m0 = 5000.;
+    /// Parameters used: CMSSM parameters
+    double m12 = 500., a0 = 0., mGutGuess = 2.0e16, tanb = 10.0, m0 = 125.;
     int sgnMu = 1;       ///< sign of mu parameter 
-    int numPoints = 500;  ///< number of scan points
+    int numPoints = 10;  ///< number of scan points
     
-    QedQcd oneset; ///< See "lowe.h" for default definitions parameters
+    QedQcd oneset;       ///< See "lowe.h" for default definitions parameters
     
     /// most important Standard Model inputs: you may change these and recompile
-    double alphasMZ = 0.1181, mtop = 173.34, mbmb = 4.18;
+    double alphasMZ = 0.1187, mtop = 173.5, mbmb = 4.18;
     oneset.setAlpha(ALPHAS, alphasMZ);
     oneset.setPoleMt(mtop);
     oneset.setMbMb(mbmb);
@@ -51,58 +52,36 @@ int main() {
 	 << TOLERANCE << endl;
     
     /// Print out header line
-    cout << "# mQEDxQCD   mh           mA           mH0          mH+-         mzrun        mwrun        mtrun        mA0run       mh0run\n";
+    cout << "# tan beta   mh           mA           mH0          mH+-\n";
     
     int i;
     /// Set limits of tan beta scan
-    //    double startlnM = 1.53164e+02, endlnM = 1.547340e+02;
-    double startlnM = MZ, endlnM = 6.0e3;    
-    numHiggsMassLoops = 3;
+    double startTb = 2., endTb = 50.;
     
     /// Cycle through different points in the scan
     for (i = 0; i <= numPoints; i++) {
-      double mScale =
-			  (endlnM - startlnM) / static_cast<double>(numPoints) *
-			  static_cast<double>(i) + startlnM
-			  ; /// set mScale ready for the scan.
+      tanb = (endTb - startTb) / static_cast<double>(numPoints) *
+	static_cast<double>(i) + startTb;  /// set tan beta ready for the scan.
 
-      QedQcd twoset(oneset);
-      twoset.runto(mScale);
+    /// Preparation for calculation: set up object and input parameters
+    MssmSoftsusy r;
+    
+    DoubleVector pars(3);
+    pars(1) = m0; pars(2) = m12; pars(3) = a0;
+    bool uni = true;  ///< MGUT defined by g1(MGUT)=g2(MGUT)
+    
+    /// Calculate the spectrum
+    r.lowOrg(sugraBcs, mGutGuess, pars, sgnMu, tanb, oneset, uni);
 
-      /// Preparation for calculation: set up object and input parameters
-      USE_TWO_LOOP_GAUGE_YUKAWA = true;      
-      MssmSoftsusy r;
-      r.setLoops(3);     
-      r.included_thresholds = 31; ///< all thresholds included
-      
-      DoubleVector pars(3);
-      pars(1) = m0; pars(2) = m12; pars(3) = a0;
-      bool uni = true;  ///< MGUT defined by g1(MGUT)=g2(MGUT)
-      
-      /// Calculate the spectrum
-      r.lowOrg(sugraBcs, mGutGuess, pars, sgnMu, tanb, twoset, uni);
-
-      r.runto(mScale);
-      r.calcDrBarPars();
-
-      double mtrun = r.displayDrBarPars().mt;
-
-
-      
-      /// check the point in question is problem free: if so print the output
-      if (!r.displayProblem().test()) 
-	cout << mScale << " " << r.displayPhys().mh0(1) << " " 
-	     << r.displayPhys().mA0(1) << " " 
-	     << r.displayPhys().mh0(2) << " " 
-	     << r.displayPhys().mHpm << " "
-	     << r.displayMzRun() << " "
-	     << r.displayMwRun() << " "
-	     << mtrun << " " 
-	     << r.printLongDrbar()
-	     << endl;
-      else
-	/// print out what the problem(s) is(are)
-	cout << "#" << mScale << " " << r.displayProblem() << endl;
+    /// check the point in question is problem free: if so print the output
+    if (!r.displayProblem().test()) 
+      cout << tanb << " " << r.displayPhys().mh0(1) << " " 
+           << r.displayPhys().mA0(1) << " " 
+           << r.displayPhys().mh0(2) << " " 
+           << r.displayPhys().mHpm << endl;
+    else
+      /// print out what the problem(s) is(are)
+      cout << tanb << " " << r.displayProblem() << endl;
   }
   }
   catch(const string & a) { cerr << a; return -1; }
